967600bf475c0f04923e9d550c4973cf1ffe6a14
rivailuthfi <rivailuthfi5@gmail.com>
2023-07-10 22:33:59 +0700

    4th commit untuk UAS

--- app/src/main/java/com/example/ujianandroid/MainActivity.java
+++ app/src/main/java/com/example/ujianandroid/MainActivity.java
@@ -41,19 +41,34 @@ public class MainActivity extends AppCompatActivity {
                     String nama_lengkap = isian_nama_depan.concat(" ").concat(isian_nama_belakang);
                     daftar_nama.clear();
 
-                    for (int i = 0; i< doubleisian_nama_umur; i++){
-                        daftar_nama.add(nama_lengkap);
+                    for (int i = 0; i < doubleisian_nama_umur; i++) {
+                        String status;
+//                        pengkondisian umur by Bagoes
+                        if (doubleisian_nama_umur >= 0 && doubleisian_nama_umur <= 10) {
+                            status = "anak";
+                        } else if (doubleisian_nama_umur >= 11 && doubleisian_nama_umur <= 20) {
+                            status = "remaja";
+                        } else if (doubleisian_nama_umur >= 21 && doubleisian_nama_umur <= 30) {
+                            status = "dewasa";
+                        } else {
+                            status = "tua";
+                        }
+
+                        if (i % 2 == 0) {
+                            // Menambahkan angka incrementing dan status di belakang nama hanya untuk nomor genap
+                            String nama_dengan_status = i + " " + nama_lengkap + ". Status : " + status;
+                            daftar_nama.add(nama_dengan_status);
+                        }
                     }
 
-
-                    daftar_nama.add(nama_lengkap);
                     edNamaDepan.setText("");
                     edNamaBelakang.setText("");
+                    edUmur.setText("");
 
                     intent_list.putStringArrayListExtra("daftar_nama", daftar_nama);
                     startActivity(intent_list);
                 }
             }
-        });
-    }
+   });
+}
 }
